1ae06b4bed69ed0cac60eee69ebf2003c821a6bb
dicksonmuli <dicksonmully6@gmail.com>
2017-05-27 13:14:51 +0300

    add sign post - to construct oauth_signature parameter required by the Yelp API before we send the request

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/YelpService.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/YelpService.java
@@ -14,6 +14,8 @@ import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 public class YelpService {
     public void findRestaurants(String location, Callback callback) {
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
+        consumer.setTokenWithSecret(Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
 
 //        request using the created url
     }
