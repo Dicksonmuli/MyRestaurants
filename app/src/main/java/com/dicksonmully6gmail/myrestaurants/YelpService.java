9722382132be1a170b81230ff0e03b0c0e057d09
dicksonmuli <dicksonmully6@gmail.com>
2017-05-27 13:12:21 +0300

    YelpService file to handle the sending and receaving http requests

---
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/YelpService.java
@@ -0,0 +1,20 @@
+package com.dicksonmully6gmail.myrestaurants;
+
+
+
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
+/**
+ * Created by dickson on 5/27/17.
+ */
+
+public class YelpService {
+    public void findRestaurants(String location, Callback callback) {
+
+//        request using the created url
+    }
+}
