bf9d5cd8c3cd03f377f7e8c8c188b2a74dbddfcf
dicksonmuli <dicksonmully6@gmail.com>
2017-05-31 11:55:00 +0300

    RestaurantDetailActivity - pull out our ArrayList<Restaurant> Parcelable using the unwrap() method

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantDetailActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantDetailActivity.java
@@ -1,8 +1,41 @@
 package com.dicksonmully6gmail.myrestaurants.ui;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dicksonmully6gmail.myrestaurants.R;
+import com.dicksonmully6gmail.myrestaurants.adapters.RestaurantPagerAdapter;
+import com.dicksonmully6gmail.myrestaurants.models.Restaurant;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by dickson on 5/30/17.
  */
 
-public class RestaurantDetailActivity {
+public class RestaurantDetailActivity extends AppCompatActivity{
+    @Bind(R.id.viewPager) ViewPager mViewPager;
+    private RestaurantPagerAdapter adapterViewPager;
+    ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+        ButterKnife.bind(this);
+
+//        pull out our ArrayList<Restaurant> Parcelable using the unwrap() method
+        mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new RestaurantPagerAdapter(getSupportFragmentManager(), mRestaurants);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
+    }
 }
