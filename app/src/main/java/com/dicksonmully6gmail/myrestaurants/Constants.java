35d8b07a5c480a900461ee78d29051d004d38b24
dicksonmuli <dicksonmully6@gmail.com>
2017-06-01 16:36:30 +0300

    adding a constant to save to shared preferences

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/Constants.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
         public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
         public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=food";
         public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+//        a constant to save to shared preferences
+        public static final String PREFERENCES_LOCATION_KEY = "location";
 }
