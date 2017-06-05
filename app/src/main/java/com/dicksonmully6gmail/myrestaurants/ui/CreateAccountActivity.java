4f927a236d8996414085aa8847630b1ea9169a7b
dicksonmuli <dicksonmully6@gmail.com>
2017-06-05 16:24:18 +0300

    add user signup layout

---
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/CreateAccountActivity.java
@@ -0,0 +1,10 @@
+package com.dicksonmully6gmail.myrestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dicksonmully6gmail.myrestaurants.R;
+
+public class CreateAccountActivity {
+
+}
---
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.dicksonmully6gmail.myrestaurants.ui;
+
+/**
+ * Created by dickson on 6/5/17.
+ */
+
+public class LoginActivitygit add . {
+}
