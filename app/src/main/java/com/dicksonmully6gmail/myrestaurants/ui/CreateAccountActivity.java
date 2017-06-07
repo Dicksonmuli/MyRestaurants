d1b39873e4a27fa09e876dfb71d820b1378bd32e
dicksonmuli <dicksonmully6@gmail.com>
2017-06-07 16:36:37 +0300

    validating registration credentials- validation methods to ensure our users' credentials are valid and reasonably secure, and error handling if they are not.

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/CreateAccountActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -117,4 +118,35 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
         };
     }
+/**
+    * validating registration credentials
+    * validation methods to ensure our users' credentials are valid
+    * and reasonably secure, and error handling if they are not.
+ */
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail = (email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEmailEditText.setError("Please enter a valid email address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+    private boolean isValidName(String name) {
+        if (name.equals("")) {
+            mNameEditText.setError("Please enter your name");
+            return false;
+        }
+        return true;
+    }
+    private boolean isValidPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mPasswordEditText.setError("Password too short");
+            return false;
+        }else if (!password.equals(confirmPassword)) {
+            mPasswordEditText.setError("Passwords do not match");
+            return false;
+        }
+        return true;
+    }
+
 }
