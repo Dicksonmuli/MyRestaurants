56e4d9c29f7128f8ede1bc7db16f7ee29aa0c24f
dicksonmuli <dicksonmully6@gmail.com>
2017-06-11 09:33:00 +0300

    creating firebase restaurant list adapter class extending FirebaseRecyclerAdapter and implement ItemTouchHelperAdapter interface and creating a contructor with Database reference

---
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -0,0 +1,31 @@
+package com.dicksonmully6gmail.myrestaurants.adapters;
+
+import android.content.Context;
+
+import com.dicksonmully6gmail.myrestaurants.models.Restaurant;
+import com.dicksonmully6gmail.myrestaurants.util.ItemTouchHelperAdapter;
+import com.dicksonmully6gmail.myrestaurants.util.OnStartDragListener;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+/**
+ * Created by dickson on 6/11/17.
+ */
+
+public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder>
+ implements ItemTouchHelperAdapter{
+
+    private DatabaseReference mRef;
+    private OnStartDragListener mOnStartDragListener;
+    private Context mContext;
+
+    public FirebaseRestaurantListAdapter(Class<Restaurant> modelClass, int modelLayout,
+                                         Class<FirebaseRestaurantViewHolder> viewHolderClass,
+                                         Query ref, OnStartDragListener onStartDragListener, Context context) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+        mRef = ref.getRef();//returns the DatabaseReference
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+    }
+}
