6ba9efee6fae8c1ca2dd897e1016672fc1d0f5cf
dicksonmuli <dicksonmully6@gmail.com>
2017-06-11 22:57:57 +0300

    adding Restaurants with ChildEventListener

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -9,8 +9,13 @@ import com.dicksonmully6gmail.myrestaurants.models.Restaurant;
 import com.dicksonmully6gmail.myrestaurants.util.ItemTouchHelperAdapter;
 import com.dicksonmully6gmail.myrestaurants.util.OnStartDragListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
 
 /**
  * Created by dickson on 6/11/17.
@@ -23,6 +28,9 @@ public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Resta
     private OnStartDragListener mOnStartDragListener;
     private Context mContext;
 
+    private ChildEventListener mChildEventListener;
+    private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+
     public FirebaseRestaurantListAdapter(Class<Restaurant> modelClass, int modelLayout,
                                          Class<FirebaseRestaurantViewHolder> viewHolderClass,
                                          Query ref, OnStartDragListener onStartDragListener, Context context) {
@@ -30,6 +38,34 @@ public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Resta
         mRef = ref.getRef();//returns the DatabaseReference
         mOnStartDragListener = onStartDragListener;
         mContext = context;
+
+//        adding Restaurants with ChildEventListener
+        mChildEventListener = mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                mRestaurants.add(dataSnapshot.getValue(Restaurant.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
     @Override
     protected void populateViewHolder(final FirebaseRestaurantViewHolder viewHolder, Restaurant model, int position) {
