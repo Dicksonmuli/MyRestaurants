7d7953a709cf902502cc075a3f35211077bac7f2
dicksonmuli <dicksonmully6@gmail.com>
2017-06-11 08:57:30 +0300

    OnStartDragListener interface - with a method onStartDrag() which will be called when the user begins a 'drag-and-drop' interaction with the touchscreen

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/util/OnStartDragListener.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/util/OnStartDragListener.java
@@ -1,8 +1,11 @@
 package com.dicksonmully6gmail.myrestaurants.util;
 
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by dickson on 6/11/17.
  */
 
-public class OnStartDragListener {
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
