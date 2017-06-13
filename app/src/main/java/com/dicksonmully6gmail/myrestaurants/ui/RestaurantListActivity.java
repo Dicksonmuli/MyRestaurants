24f9a0677121811fca9e5920dcb2c95cf5cabee8
dicksonmuli <dicksonmully6@gmail.com>
2017-06-13 11:19:30 +0300

    refactored RestaurantListActivity to have list content on RestaurantListFragment

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantListActivity.java
@@ -34,138 +34,13 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RestaurantListActivity extends AppCompatActivity {
-//    without butterknife
-//    private TextView mLocationTextView;
-//    private ListView mListView;
-
-//    @Bind(R.id.locationTextView) TextView mLocationTextView;
-//    @Bind(R.id.listView) ListView mListView;
-
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
-    private String mRecentAddress;
-
-    public static final String TAG = RestaurantListActivity.class.getSimpleName();
-
-    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
-    private RestaurantListAdapter mAdapter;
-//
-    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
-
+//refactored to have list content on RestaurantListFragment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
 //
-        ButterKnife.bind(this);
-
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
-//        mLocationTextView.setText("Here are all the restaurants near: " + location);
-
-        getRestaurants(location);
-
-//        testing shared preferences
-            mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-            mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
-            if (mRecentAddress != null) {
-                getRestaurants(mRecentAddress);
-            };
 
     }
-//    options menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
-        ButterKnife.bind(this);
-
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
-
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            when the query is submitted
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                addToSharedPreferences(query);
-                getRestaurants(query);
-                return false;
-            }
-//            when there is a noticable change on the search texteditor
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-    //callback method for req and res
-    private void getRestaurants(String location) {
-        final YelpService yelpService = new YelpService();
-        yelpService.findRestaurants(location, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-            //overriding onResponse and save data in logcat(for now)
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-//                calling runOnUiThread() method and override its run()
-                try {
-                    mRestaurants = yelpService.processResults(response);
-                }catch (StringIndexOutOfBoundsException e) {
-                    System.out.print("LOADING ....");
-                }
 //
-//
-                RestaurantListActivity.this.runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-
-                        mAdapter = new RestaurantListAdapter(getApplicationContext(), mRestaurants);
-                        mRecyclerView.setAdapter(mAdapter);
-                        RecyclerView.LayoutManager layoutManager =
-                                new LinearLayoutManager(RestaurantListActivity.this);
-                        mRecyclerView.setLayoutManager(layoutManager);
-                        mRecyclerView.setHasFixedSize(true);
-
-                    }
-
-//                        String[] restaurantNames = new String[mRestaurants.size()];
-//                        for (int i = 0; i < restaurantNames.length; i++) {
-//                            restaurantNames[i] = mRestaurants.get(i).getName();
-//                        }
-//                        ArrayAdapter adapter = new ArrayAdapter(RestaurantListActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
-//                        mListView.setAdapter(adapter);
-//
-//                        for (Restaurant restaurant : mRestaurants) {
-//                            Log.d(TAG, "Name: " + restaurant.getName());
-//                            Log.d(TAG, "Phone: " + restaurant.getPhone());
-//                            Log.d(TAG, "Website: " + restaurant.getWebsite());
-//                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-//                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-//                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-//                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
-//                        }
-//                    }
-                });
-
-
-            }
-        });
-    }
-//    adding to shared preferences
-    private void addToSharedPreferences(String location) {
-        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-    }
 }
