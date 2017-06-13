818b3f4d2bf5ec57367c1797f63682f2f0823a27
dicksonmuli <dicksonmully6@gmail.com>
2017-06-13 10:00:54 +0300

    defining mSharedPreferences (to hold searched restaurants), mEditor and setting setHasOptionsMenu to true which instructs fragment to include menu options

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantListFragment.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantListFragment.java
@@ -1,28 +1,48 @@
 package com.dicksonmully6gmail.myrestaurants.ui;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dicksonmully6gmail.myrestaurants.R;
+import com.dicksonmully6gmail.myrestaurants.adapters.RestaurantListAdapter;
+import com.dicksonmully6gmail.myrestaurants.models.Restaurant;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RestaurantListFragment extends Fragment {
-
+    //member variables
+    private RestaurantListAdapter mAdapter;
+    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentAddress;
 
     public RestaurantListFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mEditor = mSharedPreferences.edit();
+        //instructs fragment to include menu options
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_restaurant_list, container, false);
     }
