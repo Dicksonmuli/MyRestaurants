7a63dfec8db9cafe332006ded3c04252e5fd9b83
dicksonmuli <dicksonmully6@gmail.com>
2017-06-13 10:46:47 +0300

    creating options menu - * Method is now void, menu inflater is now passed in as argument

--- app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantListFragment.java
+++ app/src/main/java/com/dicksonmully6gmail/myrestaurants/ui/RestaurantListFragment.java
@@ -5,9 +5,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -65,6 +70,33 @@ public class RestaurantListFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return view;
+    }
+    @Override
+    // Method is now void, menu inflater is now passed in as argument:
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        // Call super to inherit method from parent:
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getRestaurants(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
     public void getRestaurants(String location) {
         final YelpService yelpService = new YelpService();
